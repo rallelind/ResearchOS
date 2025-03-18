ae31a6d3352ce4d4d4e923629114febc12125b41
Rasmus lind <rasmus1234555@hotmail.dk>
2025-03-18 17:20:55 +0100

    pixels

--- src/pages/Paper.tsx
+++ src/pages/Paper.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useGetPaper } from "../hooks/discover-papers/useGetPaper";
+import { LibraryBigIcon, ListIcon, ZapIcon } from "lucide-react";
 
 export function PaperPage() {
   const { paperId } = useParams();
@@ -13,21 +14,25 @@ export function PaperPage() {
   return (
     <div className="max-w-prose flex flex-col gap-4">
       <h1 className="text-2xl">{paper.title}</h1>
-      <div>
-        <p className="text-sm text-zinc-500">
-          <span className="bg-purple-100 text-purple-800 px-2 py-1 rounded-md text-xs">
-            TL;DR
-          </span>{" "}
-          {paper.tldr.text}
-        </p>
-      </div>
-      <div>
-        <p className="text-sm text-zinc-500">
-          <span className="bg-blue-100 text-blue-800 px-2 py-1 rounded-md text-xs">
-            Abstract
-          </span>{" "}
-          {paper.abstract}
-        </p>
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-1">
+            <ZapIcon className="w-4 h-4 text-purple-500" />
+            <p className="text-sm bg-purple-50 text-purple-500 px-1 rounded-md w-fit">
+              TL;DR
+            </p>
+          </div>
+          <p className="text-sm text-zinc-500">{paper.tldr.text}</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-1">
+            <LibraryBigIcon className="w-4 h-4 text-purple-500" />
+            <p className="text-sm bg-blue-50 text-blue-500 px-1 rounded-md w-fit">
+              Abstract
+            </p>
+          </div>
+          <p className="text-sm text-zinc-500">{paper.abstract}</p>
+        </div>
       </div>
     </div>
   );
