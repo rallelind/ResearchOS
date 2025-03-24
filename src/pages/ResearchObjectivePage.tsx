7d945e959b91d02b629cf6015494511e468502e5
Rasmus lind <rasmus1234555@hotmail.dk>
2025-03-24 16:01:06 +0100

    load

--- src/pages/ResearchObjectivePage.tsx
+++ src/pages/ResearchObjectivePage.tsx
@@ -3,8 +3,11 @@ import { useGetResearchObjective } from "../hooks/focused-research/research-obje
 import { ResearchObjectiveEditor } from "../components/focused-research/research-objectives/ResearchObjectiveEditor";
 import { Button } from "@headlessui/react";
 import { CircleFadingArrowUp } from "lucide-react";
+import { Spinner } from "../components/ui/Spinner";
+import { useState } from "react";
 
 export function ResearchObjectivePage() {
+  const [isAnalyzing, setIsAnalyzing] = useState(false);
   const { researchObjectiveId } = useParams();
 
   const { researchObjective } = useGetResearchObjective(
@@ -22,12 +25,18 @@ export function ResearchObjectivePage() {
         />
       </div>
       <div className="basis-1/3">
-        <Button disabled className="flex items-center gap-2 bg-black text-white p-2 px-4 rounded-full ml-auto mb-4 text-xs disabled:opacity-60 disabled:cursor-not-allowed">
+        <Button
+          disabled
+          className="flex items-center gap-2 bg-black text-white p-2 px-4 rounded-full ml-auto mb-4 text-xs disabled:opacity-60 disabled:cursor-not-allowed"
+        >
           <CircleFadingArrowUp className="w-4 h-4" />
           <p>Publish changes</p>
         </Button>
         <div className="bg-zinc-50 rounded-lg p-4 h-fit flex flex-col gap-4">
-          <p className="text-xs font-medium">Research Objectives</p>
+          <div className="flex items-center justify-between">
+            <p className="text-xs font-medium">Research Objectives</p>
+            {isAnalyzing && <Spinner size="sm" />}
+          </div>
           <p className="text-xs text-zinc-500 italic">
             Start writing your research objective and what you want researched
             to see the results.
